9533bd5cb3521a2d200f961c28ff37a5d9f5724f
Camilo Bonilla <camilobonillab26@gmail.com>
2026-03-06 18:37:45 -0500

    Nav

--- Frontend/frontend/src/app/app.ts
+++ Frontend/frontend/src/app/app.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { CatalogComponent } from './pages/catalog/catalog';
+import { NavbarComponent } from './components/navbar/navbar';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CatalogComponent],
+  imports: [CatalogComponent, NavbarComponent],
   templateUrl: './app.html',
   styleUrl: './app.scss',
 })
