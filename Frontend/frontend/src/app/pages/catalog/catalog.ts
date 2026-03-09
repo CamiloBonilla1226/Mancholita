b8e306b80ba4347aa199320690b12384f0ef6a56
Camilo Bonilla <camilobonillab26@gmail.com>
2026-03-08 22:20:36 -0500

    Configuracion en la paginacion

--- Frontend/frontend/src/app/pages/catalog/catalog.ts
+++ Frontend/frontend/src/app/pages/catalog/catalog.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, ChangeDetectorRef, Input } from '@angular/core';
+import { Component, OnInit, ChangeDetectorRef, Input, OnChanges } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ProductService } from '../../services/products.service';
 import { Product } from '../../models/product';
@@ -11,11 +11,13 @@ import { CartService } from '../../services/cart.service';
   templateUrl: './catalog.html',
   styleUrl: './catalog.scss'
 })
-export class CatalogComponent implements OnInit {
+export class CatalogComponent implements OnInit, OnChanges {
 
   @Input() searchText = '';
   @Input() selectedGender = 0;
   @Input() selectedCategory = 0;
+  currentPage = 1;
+  pageSize = 6;
 
 
   products: Product[] = [];
@@ -61,5 +63,31 @@ export class CatalogComponent implements OnInit {
 
     return filtered;
   }
+
+  get paginatedProducts(): Product[] {
+    const start = (this.currentPage - 1) * this.pageSize;
+    const end = start + this.pageSize;
+    return this.filteredProducts.slice(start, end);
+  }
+
+  get totalPages(): number {
+    return Math.ceil(this.filteredProducts.length / this.pageSize);
+  }
+
+  nextPage() {
+    if (this.currentPage < this.totalPages) {
+      this.currentPage++;
+    }
+  }
+
+  prevPage() {
+    if (this.currentPage > 1) {
+      this.currentPage--;
+    }
+  }
+  ngOnChanges() {
+  this.currentPage = 1;
+  }
+
 
 }
