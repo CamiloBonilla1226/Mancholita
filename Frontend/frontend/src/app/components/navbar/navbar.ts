03500c7b64cd3b7b3d1b7ebb681a7effed8a07b8
Camilo Bonilla <camilobonillab26@gmail.com>
2026-03-11 18:42:00 -0500

    Cambios en el Nav

--- Frontend/frontend/src/app/components/navbar/navbar.ts
+++ Frontend/frontend/src/app/components/navbar/navbar.ts
@@ -1,4 +1,12 @@
-import { Component, DoCheck, EventEmitter, OnInit, Output, HostListener, ChangeDetectorRef } from '@angular/core';
+import {
+  Component,
+  DoCheck,
+  EventEmitter,
+  OnInit,
+  Output,
+  HostListener,
+  ChangeDetectorRef
+} from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { CartService } from '../../services/cart.service';
@@ -15,46 +23,32 @@ export class NavbarComponent implements DoCheck, OnInit {
 
   cartCount = 0;
   searchText = '';
-  lastScrollTop = 0;
-  isVisible = true;
-  cartBump = false;
 
   categories: CategoryNode[] = [];
   subcategories: CategoryNode[] = [];
+
   selectedGender = 0;
+  selectedCategory = 0;
+
+  cartBump = false;
+  previousCount = 0;
+
+  lastScrollTop = 0;
+  isVisible = true;
 
   @Output() cartClicked = new EventEmitter<void>();
   @Output() searchChanged = new EventEmitter<string>();
   @Output() genderChanged = new EventEmitter<number>();
   @Output() categoryChanged = new EventEmitter<number>();
-  @HostListener('window:scroll', [])
+  @Output() logoClicked = new EventEmitter<void>();
 
 
-@HostListener('window:scroll', [])
-onWindowScroll() {
-  const currentScroll = window.pageYOffset || document.documentElement.scrollTop;
+  constructor(
+    private cartService: CartService,
+    private categoryService: CategoryService,
+    private cdr: ChangeDetectorRef
+  ) { }
 
-  if (currentScroll <= 0) {
-    this.isVisible = true;
-    this.lastScrollTop = 0;
-    return;
-  }
-
-  if (currentScroll > this.lastScrollTop && currentScroll > 80) {
-    // bajando
-    this.isVisible = false;
-  } else if (currentScroll < this.lastScrollTop) {
-    // subiendo
-    this.isVisible = true;
-  }
-
-  this.lastScrollTop = currentScroll;
-}
- constructor(
-  private cartService: CartService,
-  private categoryService: CategoryService,
-  private cdr: ChangeDetectorRef
-) {}
   ngOnInit(): void {
     this.categoryService.getCategories().subscribe({
       next: (data) => {
@@ -65,19 +59,36 @@ onWindowScroll() {
       }
     });
   }
-
-  previousCount = 0;
-
-ngDoCheck(): void {
-  const currentCount = this.cartService.getCount();
-  this.cartCount = currentCount;
-
-  if (currentCount > this.previousCount) {
-    this.animateCart();
+  goToHome() {
+    this.logoClicked.emit();
   }
 
-  this.previousCount = currentCount;
-}
+  ngDoCheck(): void {
+    const currentCount = this.cartService.getCount();
+    this.cartCount = currentCount;
+
+    if (currentCount > this.previousCount) {
+      this.animateCart();
+    }
+
+    this.previousCount = currentCount;
+  }
+
+  animateCart() {
+    this.cartBump = false;
+    this.cdr.detectChanges();
+
+    setTimeout(() => {
+      this.cartBump = true;
+      this.cdr.detectChanges();
+
+      setTimeout(() => {
+        this.cartBump = false;
+        this.cdr.detectChanges();
+      }, 300);
+    }, 20);
+  }
+
   openCart() {
     this.cartClicked.emit();
   }
@@ -88,35 +99,36 @@ ngDoCheck(): void {
 
   filterByGender(genderId: number) {
     this.selectedGender = genderId;
-    this.genderChanged.emit(genderId);
+    this.selectedCategory = 0;
 
-    if (genderId === 0) {
-      this.subcategories = [];
-      this.categoryChanged.emit(0);
-      return;
-    }
+    this.genderChanged.emit(genderId);
+    this.categoryChanged.emit(0);
 
     const gender = this.categories.find(c => c.id === genderId);
     this.subcategories = gender?.children ?? [];
-    this.categoryChanged.emit(0);
   }
 
   filterByCategory(categoryId: number) {
+    this.selectedCategory = categoryId;
     this.categoryChanged.emit(categoryId);
   }
 
- animateCart() {
-  this.cartBump = false;
-  this.cdr.detectChanges();
+  @HostListener('window:scroll', [])
+  onWindowScroll() {
+    const currentScroll = window.pageYOffset || document.documentElement.scrollTop;
 
-  setTimeout(() => {
-    this.cartBump = true;
-    this.cdr.detectChanges();
+    if (currentScroll <= 0) {
+      this.isVisible = true;
+      this.lastScrollTop = 0;
+      return;
+    }
 
-    setTimeout(() => {
-      this.cartBump = false;
-      this.cdr.detectChanges();
-    }, 300);
-  }, 20);
-}
+    if (currentScroll > this.lastScrollTop && currentScroll > 80) {
+      this.isVisible = false;
+    } else if (currentScroll < this.lastScrollTop) {
+      this.isVisible = true;
+    }
+
+    this.lastScrollTop = currentScroll;
+  }
 }
